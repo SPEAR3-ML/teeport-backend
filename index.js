bc113e05c79011a5e57ee95ddaaa650b051f67f8
Zhe Zhang <zhezhang@slac.stanford.edu>
2019-10-30 19:18:17 -0700

    Add some test code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const wss = new WebSocket.Server({ port: 8080 })
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     console.log('received: %s', message)
+    ws.send('end')
   })
  
   ws.send('something')
