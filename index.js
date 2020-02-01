ad721cf26a743c8b52de1045b7b2ca2b697985ef
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-01-31 17:25:35 -0800

    Minor visual modification

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const version = '0.1'
 const serve = async () => {
   console.log(`================== SPEAR3 Platform v${version} ==================`)
   const current = new Date()
-  console.log(`Service starting time : ${df(current, 'yyyy-mm-dd HH:MM:ss')}`)
+  console.log(`Service starting time : ${df(current, 'yyyy-mm-dd HH:MM:ss')}\n`)
 
   const configs = {
     taskServer: {
