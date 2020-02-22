a2fa2d313a86fd18eceb34771085226896f583a1
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-02-21 17:52:17 -0800

    Change the getClients API from returning obj to list And sorted by connectedAt property by default

--- src/task-server/logics/client.js
+++ src/task-server/logics/client.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const store = require('../redux/store')
 const {
   selectClients,
@@ -5,9 +7,14 @@ const {
 
 const getClients = (msg, ws, server, logger) => {
   const clients = selectClients(store.getState())
+  const sortedClients = _.toPairs(clients).map(([clientId, client]) => {
+    return _.assign(client, { id: clientId })
+  }).sort((c1, c2) => {
+    return c2.connectedAt - c1.connectedAt
+  })
   const res = {
     type: 'clients',
-    clients,
+    clients: sortedClients,
   }
   ws.send(JSON.stringify(res))
 }
