5cf57af8f8529bdbd858551fa1b776943cd93291
Zhe Zhang <zhezhang@slac.stanford.edu>
2020-09-10 00:20:15 -0700

    Bump up the version number And make port an env var, also show port in the server starting screen

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const df = require('dateformat')
 
-const version = '0.2.0'
+const version = '0.3.0'
 const logLevel = process.env.NODE_ENV === 'development' ? 'debug' : 'info'
+const port = parseInt(process.env.NODE) || 8080
 
 const serve = async () => {
   console.log(`================== Teeport Server v${version} ==================`)
   const current = new Date()
   console.log(`Service starting time : ${df(current, 'yyyy-mm-dd HH:MM:ss')}`)
   console.log(`Service mode          : ${process.env.NODE_ENV}`)
-  console.log(`Service log level     : ${logLevel}\n`)
+  console.log(`Service log level     : ${logLevel}`)
+  console.log(`Service port          : ${port}\n`)
 
   const configs = {
     taskServer: {
-      port: 8080,
+      port,
     },
     taskLog: {
       level: logLevel,
